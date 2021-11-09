95965561941e11a820a56138864dfe4d3afa49e3
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-09 16:35:54 +0000

    fix slow training

--- src/train.py
+++ src/train.py
@@ -14,13 +14,14 @@ from utils.utils import get_paths, dotdict, init_model, parse_arguments, get_ful
 from src.configs import args as default_args
 from pytorch_lightning import seed_everything
 
-#ROOT_DIR = os.path.dirname(__file__)  # folder containing this file
+
+# ROOT_DIR = os.path.dirname(__file__)  # folder containing this file
 
 def setup_experiment(model, train_loader, valid_loader, checkpoints, args):
     # setup lightining model params
     params = dict(model=model, lr=args.lr, weight_decay=args.conv_reg, loss_type=args.loss_type,
                   num_outputs=args.num_outputs, metric='r2')
-  
+
     # setting experiment_path
     experiment = get_full_experiment_name(args.experiment_name, args.batch_size,
                                           args.fc_reg, args.conv_reg, args.lr)
@@ -28,7 +29,6 @@ def setup_experiment(model, train_loader, valid_loader, checkpoints, args):
     dirpath = os.path.join(args.out_dir, 'dhs_ooc', experiment)
     print(f'checkpoints directory: {dirpath}')
     os.makedirs(dirpath, exist_ok=True)  # check if it can be created automatically
-    
 
     # logger
     logger = TensorBoardLogger(os.path.join(args.out_dir, f"{args.model_name}_logs"), name=args.model_name)
@@ -39,19 +39,20 @@ def setup_experiment(model, train_loader, valid_loader, checkpoints, args):
                                           filename="{args.model_name}-{epoch:02d}-{args.monitor:.2f}",
                                           dirpath=dirpath,
                                           verbose=True,
-                                          save_top_k=1,
+                                          # save_top_k=1,
                                           # sanity check
                                           save_last=True)
 
-    trainer = pl.Trainer(max_epochs=args.max_epochs, gpus=args.no_of_gpus ,
+    trainer = pl.Trainer(max_epochs=args.max_epochs, gpus=args.no_of_gpus,
                          logger=logger,
                          callbacks=[checkpoint_callback],
                          resume_from_checkpoint=args.resume,
-                         #precision=16,
+                         # precision=16,
                          limit_train_batches=0.25,
-                         #distributed_backend='ddp',
+                         # distributed_backend='ddp',
                          profiler='simple',
-                         accumulate_grad_batches=16, )  # understand what it does exactly
+                         flush_logs_every_n_steps=50)
+    # accumulate_grad_batches=16, )  # understand what it does exactly
     if checkpoints:
         print(f'Initializing using pretrained lightning model:\n{checkpoints}')
         pretrained_model = ResTrain(**params)
@@ -61,12 +62,12 @@ def setup_experiment(model, train_loader, valid_loader, checkpoints, args):
     trainer.fit(litmodel, train_loader, valid_loader)
 
     torch.save(litmodel.model.state_dict(),
-              dirpath)  # save the model itself (resnetms for example)rather than saving the lighting model
+               dirpath)  # save the model itself (resnetms for example)rather than saving the lighting model
 
     best_model_ckpt = checkpoint_callback.best_model_path
     best_model_score = checkpoint_callback.best_model_score
 
-    return best_model_ckpt, best_model_score,dirpath
+    return best_model_ckpt, best_model_score, dirpath
 
 
 def main(args):
@@ -75,8 +76,9 @@ def main(args):
     paths_train = get_paths(args.dataset, ['train'], args.fold, args.data_path)
     paths_valid = get_paths(args.dataset, ['val'], args.fold, args.data_path)
 
-    #save data_params for later use
-    data_params = dict(dataset=args.dataset,fold=args.fold,ls_bands= args.ls_bands, nl_band=args.nl_band,label_name= args.label_name,
+    # save data_params for later use
+    data_params = dict(dataset=args.dataset, fold=args.fold, ls_bands=args.ls_bands, nl_band=args.nl_band,
+                       label_name=args.label_name,
                        nl_label=args.nl_label, batch_size=args.batch_size, groupby=args.group)
 
     batcher_train = Batcher(paths_train, args.scaler_features_keys, args.ls_bands, args.nl_band, args.label_name,
@@ -89,23 +91,25 @@ def main(args):
     # model
     ckpt, pretrained = init_model(args.model_init, args.init_ckpt_dir, )
     model = get_model(args.model_name, in_channels=args.in_channels, pretrained=pretrained, ckpt_path=ckpt)  ##TEST
-    best_model_ckpt, _ ,dirpath= setup_experiment(model, batcher_train, batcher_valid, args.checkpoints, args)
+    best_model_ckpt, _, dirpath = setup_experiment(model, batcher_train, batcher_valid, args.checkpoints, args)
     print(f'Path to best model found during training: \n{best_model_ckpt}')
 
-    #saving data_param:
+    # saving data_param:
     params_filepath = os.path.join(dirpath, 'data_params.json')
     with open(params_filepath, 'w') as config_file:
         json.dump(data_params, config_file, indent=4)
- 
+
     # saving resnet model params filepath
-    params=dict(model_name=args.model_name, in_channels=args.in_channels)
+    params = dict(model_name=args.model_name, in_channels=args.in_channels)
     params_filepath = os.path.join(dirpath, 'params.json')
     with open(params_filepath, 'w') as config_file:
         json.dump(params, config_file, indent=4)
-if __name__ == "__main__":
-        print('GPUS:',torch.cuda.device_count())
-        parser = argparse.ArgumentParser()
-        args = parse_arguments(parser, default_args)
-        args = dotdict(args)
 
-        main(args)
+
+if __name__ == "__main__":
+    print('GPUS:', torch.cuda.device_count())
+    parser = argparse.ArgumentParser()
+    args = parse_arguments(parser, default_args)
+    args = dotdict(args)
+
+    main(args)
