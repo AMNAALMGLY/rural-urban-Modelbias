1e4f74f05cd20d6cefe72e23ec715214f3f45357
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-11 00:13:50 +0000

    add torch dataloader

---
+++ batchers/torch_dataset.py
@@ -0,0 +1,44 @@
+import os.path
+from glob import glob
+import numpy as np
+import pandas as pd
+import torch.utils.data
+from torch.utils.data import Dataset
+from batchers.dataset import Batcher
+from src.configs import args
+from utils.utils import get_paths
+
+data_dir='./np_data'
+file_path=os.path.join(data_dir,'*.npz')
+all_files=glob(file_path)
+length=len(all_files)
+
+class Data(Dataset):
+    def __init__(self,data_dir):
+        def __init__(self, data_dir):
+            data = pd.DataFrame(columns=['images', 'locs', 'years', 'labels'])
+            for i in range(length):
+                print(i)
+                with np.load(os.path.join(data_dir, f'{i:05d}.npz')) as d:
+                    data.loc[i, 'images'] = d['images']
+                    data.loc[i, 'locs'] = d['locs']
+                    data.loc[i, 'years'] = d['years']
+                    data.loc[i, 'labels'] = d['labels']
+            self.data = data
+            self.x = data['images']
+            self.y = data['labels']
+
+        def __len__(self):
+            return length
+
+        def __getitem__(self, item):
+            return self.data[item]
+paths_train = get_paths(args.dataset, ['train'], args.fold, args.data_path)
+batcher_train = Batcher(paths_train, args.scaler_features_keys, args.ls_bands, args.nl_band, args.label_name,
+                            args.nl_label, 'DHS', args.augment, args.batch_size, groupby=args.group,
+                            cache=True,save_dir=data_dir)
+batcher_train.tfDatase_to_np()
+dataloader=torch.utils.data.DataLoader(Data(data_dir=data_dir),batch_size=16,num_workers=3,pin_memory=True, prefetch_factor=2,shuffle=True)
+for x, y in dataloader:
+    print(x,y)
+
