7ad0003f32c3acf2fc4fa6255416b424d05ea742
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-14 15:57:22 +0000

    fix slow training

--- src/train.py
+++ src/train.py
@@ -24,10 +24,10 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
-                                          shuffle=True, num_workers=2)
+#trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    #                                    download=True, transform=transform)
+#trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+      #                                    shuffle=True, num_workers=2)
 
 
 data_dir = './np_data'
