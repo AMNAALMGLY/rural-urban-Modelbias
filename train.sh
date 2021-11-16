1f743545164ce4e369bee88f5da35d8b8086f978
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-16 01:46:02 +0000

    fix slow training

--- train.sh
+++ train.sh
@@ -51,7 +51,7 @@ export CUDA_VISIBLE_DEVICES=0
 sbatch --output=${LOG_FOLDER}/%j.out --error=${LOG_FOLDER}/%j.err \
     --nodes=1 --ntasks-per-node=1 --time=2-00:00:00 --mem=100G \
     --partition=atlas --cpus-per-task=10 \
-    --gres=gpu:titanxp:${GPUS} --job-name=${JOBNAME} --wrap="${WRAP}"
+    --gres=gpu:${GPUS} --job-name=${JOBNAME} --wrap="${WRAP}"
 
 
 echo "All jobs launched!"
