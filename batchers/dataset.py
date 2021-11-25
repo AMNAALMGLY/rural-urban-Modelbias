63b115baaa6e3a9007f513dc173f1850f4dbf764
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-25 12:49:57 +0000

    reproduce results

--- batchers/dataset.py
+++ batchers/dataset.py
@@ -72,8 +72,9 @@ class Batcher(torch.utils.data.IterableDataset):
         self.batch_size = batch_size
         self.shuffle = shuffle
         self._iterator = None
-        self.ds = get_dataset(tfrecords, batch_size, self.tfrecords_to_dict, cache, shuffle, groupby, augment,
-                self.augment_ex)
+
+        self.ds = get_dataset(tfrecords, batch_size, label,nl_label,ls_bands,nl_bands,scalar_features_keys,clipn,normalize,cache, shuffle, groupby, augment,
+                )
 
         # TODO:check values of arguments passed
 
@@ -107,12 +108,12 @@ class Batcher(torch.utils.data.IterableDataset):
         do_keep = tf.equal(ex[self.groupby], 0.0)
         return do_keep
     '''
-
+    '''      #convert the records to dictionaries of {'feature_name' : tf.Tensor}
     def tfrecords_to_dict(self, example: tf.Tensor) -> dict[str, tf.Tensor]:
-        '''
-        convert the records to dictionaries of {'feature_name' : tf.Tensor}
+        
+   
 
-        '''
+        
         # TODO:NIGHTLIGHT band/label
 
         bands = {'rgb': ['BLUE', 'GREEN', 'RED'], 'ms': ['BLUE', 'GREEN', 'RED', 'SWIR1', 'SWIR2', 'TEMP1', 'NIR'],
@@ -190,7 +191,7 @@ class Batcher(torch.utils.data.IterableDataset):
                 result[key] = ex[key]
         print('finished converting to dict')
         return result
-
+    '''
     def tfDatase_to_np(self):
         # TODO:test, move to utils.py
         '''
@@ -253,6 +254,7 @@ class Batcher(torch.utils.data.IterableDataset):
         print(f'Time in getdataset: {time.time() - start}')
         return dataset.as_numpy_iterator()
     '''
+    '''
     def augment_ex(self, ex: dict[str, tf.Tensor],seed) -> dict[str, tf.Tensor]:
         """Performs image augmentation (random flips + levels brightnes/contrast adjustments).
           Does not perform level adjustments on NL band(s).
@@ -289,8 +291,7 @@ class Batcher(torch.utils.data.IterableDataset):
         print(img, ex['images'])
         ex['images'] = img
         return ex
-
-
+    '''
     def __iter__(self):
         '''
         implement iterator of the  loader
@@ -315,8 +316,125 @@ class Batcher(torch.utils.data.IterableDataset):
         print(f'time in next: {time.time() - start}')
         return batch
 
+def tfrecords_to_dict(label,nl_label,ls_bands,nl_bands,scalar_features_keys,clipn,normalize, example: tf.Tensor) -> dict[str, tf.Tensor]:
+        '''
+        convert the records to dictionaries of {'feature_name' : tf.Tensor}
 
-def get_dataset(tfrecords, batch_size, tfrecords_to_dict: Callable, cache, shuffle, groupby, augment,
+        '''
+        # TODO:NIGHTLIGHT band/label
+
+        bands = {'rgb': ['BLUE', 'GREEN', 'RED'], 'ms': ['BLUE', 'GREEN', 'RED', 'SWIR1', 'SWIR2', 'TEMP1', 'NIR'],
+                 'merge': ['NIGHTLIGHTS'], 'split': ['NIGHTLIGHTS'], 'center': ['NIGHTLIGHTS'], 'mean': ['NIGHTLIGHTS']}
+        keys_to_features = {}
+        scalar_float_keys = ['lat', 'lon', 'year']
+        if label is not None:
+            scalar_float_keys.append(label)
+        ex_bands = bands[ls_bands] + bands.get(nl_bands, [])
+        print('ex_bands :', ex_bands)
+        for band in ex_bands:
+            keys_to_features[band] = tf.io.FixedLenFeature(shape=[255 ** 2], dtype=tf.float32)
+
+        for key in scalar_float_keys:
+            keys_to_features[key] = tf.io.FixedLenFeature(shape=[], dtype=tf.float32)
+        if scalar_features_keys is not None:
+            for key, dtype in scalar_features_keys.items():
+                keys_to_features[key] = tf.io.FixedLenFeature(shape=[], dtype=dtype)
+
+        ex = tf.io.parse_single_example(example, features=keys_to_features)
+
+        loc = tf.stack([ex['lat'], ex['lon']])
+        year = tf.cast(ex.get('year', -1), tf.int32)
+        img = float('nan')
+        if len(ex_bands) > 0:
+            for band in ex_bands:  ##TODO is this loop necessary ?vectorize
+                ex[band].set_shape([255 * 255])
+                ex[band] = tf.reshape(ex[band], [255, 255])[15:-16, 15:-16]  # crop to 224x224
+
+                if clipn:
+                    ex[band] = tf.nn.relu(ex[band])
+                if normalize:
+                    means = MEANS_DICT[normalize]
+                    stds = STD_DEVS_DICT[normalize]
+                    if band == 'NIGHTLIGHTS' and year < 2012:
+                        ex[band] = (ex[band] - means['DMSP']) / stds['DMSP']
+                    elif band == 'NIGHTLIGHTS' and year > 2012:
+                        ex[band] = (ex[band] - means['VIIRS']) / stds['VIIRS']
+                    else:
+                        ex[band] = (ex[band] - means[band]) / stds[band]
+            # TODO augmentation
+            img = tf.stack([ex[band] for band in ex_bands], axis=2)
+            # img=tf.image.resize_with_crop_or_pad(img,32,32)
+
+
+        else:
+            raise ValueError
+
+
+        label_ms = ex.get(label, float('nan'))
+
+        if nl_label:
+            if nl_label == 'mean':
+                nl = tf.reduce_mean(ex['NIGHTLIGHTS'])
+            elif nl_label == 'center':
+                nl = ex['NIGHTLIGHTS'][112, 112]
+            else:
+                raise ValueError  ##TODO : move this to  the initialization (restrict to range of values)
+        else:
+            nl = float('nan')
+
+        if nl_label and label:
+            labels = tf.stack([label_ms, nl])
+        elif nl_label:
+            labels = nl
+        elif label:
+            labels = label_ms
+        else:
+            labels = float('nan')
+
+        result = {'images': img, 'locs': loc, 'years': year, 'labels': labels}
+
+        if scalar_features_keys:
+            for key in scalar_features_keys:
+                result[key] = ex[key]
+        print('finished converting to dict')
+        return result
+def augment_ex( ex: dict[str, tf.Tensor],seed,nl_bands,ls_bands,nl_label) -> dict[str, tf.Tensor]:
+        """Performs image augmentation (random flips + levels brightnes/contrast adjustments).
+          Does not perform level adjustments on NL band(s).
+
+          Args
+          - ex: dict {'images': img, ...}
+              - img: tf.Tensor, shape [H, W, C], type float32
+                  NL band depends on self.ls_bands and self.nl_band
+
+          Returns: ex, with img replaced with an augmented image
+          """
+        print('in augment ex')
+        img = ex['images']
+        img = tf.image.stateless_random_flip_left_right(img, seed=seed)
+        img = tf.image.stateless_random_flip_left_right(img, seed=seed)
+
+        if nl_bands and ls_bands:
+            if nl_label == 'merge':
+
+                img = tf.image.stateless_random_brightness(img[:, :, :-1], max_delta=0.5, seed=seed)
+                img = tf.image.stateless_random_contrast(img[:, :, :-1], lower=0.75, upper=1.25, seed=seed)
+                img = tf.concat([img, ex['image'][:, :, -1:]], axis=2)
+            else:
+
+                img = tf.image.stateless_random_brightness(img[:, :, :-2], max_delta=0.5, seed=seed)
+                img = tf.image.stateless_random_contrast(img[:, :, :-2], lower=0.75, upper=1.25, seed=seed)
+                img = tf.concat([img, ex['images'][:, :, -2:]], axis=2)
+
+        elif ls_bands:
+
+            img = tf.image.stateless_random_brightness(img, max_delta=0.5, seed=seed)
+            img = tf.image.stateless_random_contrast(img, lower=0.75, upper=1.25, seed=seed)
+        print('images augment')
+        print(img, ex['images'])
+        ex['images'] = img
+        return ex
+def get_dataset(tfrecords, batch_size, label,nl_label,ls_bands,nl_bands,scalar_features_keys,clipn,normalize, cache, shuffle, groupby, augment,
                 augment_ex: Callable):
     '''
         do the tf_to dict operation to the whole dataset in numpy dtype
@@ -341,7 +459,7 @@ def get_dataset(tfrecords, batch_size, tfrecords_to_dict: Callable, cache, shuff
 
     dataset = dataset.prefetch(2 * batch_size)
 
-    dataset = dataset.map(lambda ex: tfrecords_to_dict(ex), num_parallel_calls=args.num_workers)
+    dataset = dataset.map(lambda ex:  tfrecords_to_dict(label,nl_label,ls_bands,nl_bands,scalar_features_keys,clipn,normalize, ex), num_parallel_calls=args.num_workers)
     if groupby == 'urban':
 
         dataset = dataset.filter(lambda ex: tf.equal(ex['urban_rural'], 1.0))
@@ -359,7 +477,8 @@ def get_dataset(tfrecords, batch_size, tfrecords_to_dict: Callable, cache, shuff
         print('in augment')
         counter = tf.data.experimental.Counter()
         dataset = tf.data.Dataset.zip((dataset, (counter, counter)))
-        dataset = dataset.map(augment_ex, num_parallel_calls=args.num_workers)
+        seed=123
+        dataset = dataset.map(lambda ex :augment_ex( ex,seed,nl_bands,ls_bands,nl_label) , num_parallel_calls=args.num_workers)
     dataset = dataset.batch(batch_size=batch_size)
     print('in batching')
     dataset = dataset.prefetch(2)
