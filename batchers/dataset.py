782e89180751afc49fd79c51bea0dc6e9c8dc1a9
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-27 22:32:02 +0000

    Extracting Features

--- batchers/dataset.py
+++ batchers/dataset.py
@@ -218,7 +218,7 @@ class Batcher(torch.utils.data.IterableDataset):
         if self.shuffle:
             print('in shuffle')
             # shuffle the order of the input files, then interleave their individual records
-            dataset = tf.data.Dataset.from_tensor_slices(self.tfrecords).shuffle(buffer_size=1000,reshuffle_each_iteration=False).interleave(
+            dataset = tf.data.Dataset.from_tensor_slices(self.tfrecords).shuffle(buffer_size=1000,reshuffle_each_iteration=True).interleave(
                 lambda file_path: tf.data.TFRecordDataset(file_path, compression_type='GZIP'),
                 cycle_length=AUTO, block_length=1, )
 
