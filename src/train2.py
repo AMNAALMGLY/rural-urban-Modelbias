bd671fb5388a16bbb3e16e4cfd4d75b35a3a45b0
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-27 00:34:51 +0000

    Extracting Features

--- src/train2.py
+++ src/train2.py
@@ -99,11 +99,11 @@ def main(args):
 
     batcher_train = Batcher(paths_train, args.scaler_features_keys, args.ls_bands, args.nl_band, args.label_name,
                             args.nl_label, 'DHS',args.augment, args.clipn, args.batch_size, groupby=args.group,
-                            cache=True,shuffle=False)
+                            cache=False,shuffle=False)
 
     batcher_valid = Batcher(paths_valid, args.scaler_features_keys, args.ls_bands, args.nl_band, args.label_name,
                             args.nl_label, 'DHS', False, args.clipn, args.batch_size, groupby=args.group,
-                            cache=True,shuffle=False)
+                            cache=False,shuffle=False)
     batcher_test=Batcher(paths_test[:1000], args.scaler_features_keys, args.ls_bands, args.nl_band, args.label_name,
                             args.nl_label, 'DHS', False, args.clipn, args.batch_size, groupby=args.group,
                             cache=True,shuffle=False)
