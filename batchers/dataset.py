7314f5ac5cb1a190a2ff91549d85cf55a16bb4ce
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-11-29 00:03:16 +0000

    running expereiments

--- batchers/dataset.py
+++ batchers/dataset.py
@@ -223,7 +223,7 @@ class Batcher():
                  lambda file_path: tf.data.TFRecordDataset(file_path,  compression_type='GZIP',num_parallel_reads=AUTO),
                 cycle_length=5,
                 block_length=1,
-                num_parallel_calls=tf.data.experimental.AUTOTUNE
+                num_parallel_calls=AUTO
             )
 
 
@@ -235,19 +235,19 @@ class Batcher():
                 buffer_size=1024 * 1024 * 128,  # 128 MB buffer size
                 num_parallel_reads=AUTO)
 
+        dataset = dataset.prefetch(2 * self.batch_size)
         dataset = dataset.map(lambda ex: self.tfrecords_to_dict(ex), num_parallel_calls=AUTO)
 
         if self.nl_bands == 'split':
             dataset = dataset.map(self.split_nl_band)
 
-        dataset = dataset.prefetch(2 * self.batch_size)
+
 
         if self.groupby == 'urban':
 
             dataset = dataset.filter(lambda ex: tf.equal(ex['urban_rural'], 1.0))
         elif self.groupby == 'rural':
             dataset = dataset.filter(lambda ex: tf.equal(ex['urban_rural'], 0.0))
-        # dataset = dataset.prefetch(4 * self.batch_size)      #Not sure of this
 
         if cache:
             dataset = dataset.cache()
