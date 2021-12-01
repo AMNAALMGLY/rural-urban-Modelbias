f44425acdfe5b53268b5ab1a4f8dcb59a4d7e1c2
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-01 01:09:27 +0000

    change the model to preact resnet

--- models/preact_resnet.py
+++ models/preact_resnet.py
@@ -15,101 +15,172 @@ model_urls = {
     "resnet50": "https://download.pytorch.org/models/resnet50-0676ba61.pth",
 
 }
-#TODO add downsample attribute
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
 class PreActBlock(nn.Module):
-    '''Pre-activation version of the BasicBlock.'''
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, preactivate=True):
         super(PreActBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
-            )
+        self.pre_bn = self.pre_relu = None
+        if preactivate:
+            self.pre_bn = nn.BatchNorm2d(inplanes)
+            self.pre_relu = nn.ReLU(inplace=True)
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1_2 = nn.BatchNorm2d(planes)
+        self.relu1_2 = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.preactivate = preactivate
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
-        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
-        out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
-        out += shortcut
+        if self.preactivate:
+            preact = self.pre_bn(x)
+            preact = self.pre_relu(preact)
+        else:
+            preact = x
+
+        out = self.conv1(preact)
+        out = self.bn1_2(out)
+        out = self.relu1_2(out)
+        out = self.conv2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(preact)
+        else:
+            residual = x
+
+        out += residual
+
         return out
+
 
 class PreActBottleneck(nn.Module):
-    '''Pre-activation version of the original Bottleneck module.'''
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, preactivate=True):
         super(PreActBottleneck, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
-            )
+        self.pre_bn = self.pre_relu = None
+        if preactivate:
+            self.pre_bn = nn.BatchNorm2d(inplanes)
+            self.pre_relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1_2 = nn.BatchNorm2d(planes)
+        self.relu1_2 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2_3 = nn.BatchNorm2d(planes)
+        self.relu2_3 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.downsample = downsample
+        self.stride = stride
+        self.preactivate = preactivate
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
-        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
-        out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
-        out = self.conv3(F.relu(self.bn3(out)))
-        out += shortcut
+        if self.preactivate:
+            preact = self.pre_bn(x)
+            preact = self.pre_relu(preact)
+        else:
+            preact = x
+
+        out = self.conv1(preact)
+        out = self.bn1_2(out)
+        out = self.relu1_2(out)
+
+        out = self.conv2(out)
+        out = self.bn2_3(out)
+        out = self.relu2_3(out)
+
+        out = self.conv3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(preact)
+        else:
+            residual = x
+
+        out += residual
+
         return out
 
-
 class PreActResNet(nn.Module):
-    def __init__(self, block, in_channels,num_blocks, num_classes=1000):
+    def __init__(self, block, in_channels,layers, num_classes=1000):
+        self.inplanes = 64
         super(PreActResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        #self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.final_bn = nn.BatchNorm2d(512 * block.expansion)
+        self.final_relu = nn.ReLU(inplace=True)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512*block.expansion, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+            )
+
+        # On the first residual block in the first residual layer we don't pre-activate,
+        # because we take care of that (+ maxpool) after the initial conv layer
+        preactivate_first = stride != 1
+
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+        layers.append(block(self.inplanes, planes, stride, downsample, preactivate_first))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        print(out.shape)
-        out=self.avgpool(out)
-        print(out.shape)
-        out = torch.flatten(out,1)
-        out = self.fc(out)
-        return out
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.final_bn(x)
+        x = self.final_relu(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
 
 
 def PreActResNet18(in_channels,pretrained):
     model = PreActResNet(PreActBlock, in_channels,[2,2,2,2],)
-    print(model)
+
     state_dict = np.load(args.imagenet_weight_path)  # TODO put it in urls list as in resnet
     d = load_state_dict_from_url(model_urls['resnet18'])
     print(d.keys())
+    print(model.state_dict())
     if pretrained:
         state_dict = np.load(args.imagenet_weight_path) #TODO put it in urls list as in resnet
         new_dict=OrderedDict()
