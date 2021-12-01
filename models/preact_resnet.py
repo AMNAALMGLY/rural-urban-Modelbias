1f0ab346fdd9c109ecc3c126c29980cbbda1d3c1
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-01 04:11:45 +0000

    change the model to preact resnet

--- models/preact_resnet.py
+++ models/preact_resnet.py
@@ -7,9 +7,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models.alexnet import model_urls
+
 from models._internally_replaced_utils import load_state_dict_from_url
 from configs import args
-
+print(load_state_dict_from_url(model_urls['resnet18'])
 
 def load_tensor_pack(model,path,in_channels):
     tensor_pack_dict = np.load(path)  # tensor pack dict
