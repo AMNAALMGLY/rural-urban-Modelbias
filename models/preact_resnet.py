8da13accdf6d833b24ddde56187485a715578bb4
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-01 01:24:52 +0000

    change the model to preact resnet

--- models/preact_resnet.py
+++ models/preact_resnet.py
@@ -129,7 +129,7 @@ class PreActResNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.weight.data.normal_(0, np.sqrt(2. / n))
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
