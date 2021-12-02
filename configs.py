2ed0ed22fa7cd89528113edf02f7bef9f086920e
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-02 14:12:32 +0000

    run experiments with preact

--- configs.py
+++ configs.py
@@ -24,8 +24,8 @@ args = Namespace(
     patience=20,
 
     lr=.0001,                                                                                  #lr0001         #0.0001 nl,ms
-    fc_reg=.001,                #fc01_conv01_lr0001        fc001_conv001_lr0001       fc001_conv001_lr001   fc001_conv001_lr01       fc01_conv01_lr001
-    conv_reg=.001,
+    fc_reg=.01,                #fc01_conv01_lr0001        fc001_conv001_lr0001       fc001_conv001_lr001   fc001_conv001_lr01       fc01_conv01_lr001
+    conv_reg=.01,
 
     # data
 
@@ -36,7 +36,7 @@ args = Namespace(
     clipn=True,
     ooc=True,
     dataset='DHS_OOC',
-    fold='D',
+    fold='A',
     ls_bands='ms',
     nl_band=None,  # [None , merge , split]
     nl_label=None,  # [center, mean,None]
@@ -46,7 +46,7 @@ args = Namespace(
     # Experiment
 
     seed=123,
-    experiment_name='DHS_OOC_D_ms_samescaled',
+    experiment_name='DHS_OOC_A_ms_samescaled',
     out_dir=os.path.join(ROOT_DIR, 'outputs'),
     init_ckpt_dir=None,
     group=None,
