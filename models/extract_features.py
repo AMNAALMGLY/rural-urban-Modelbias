79454f8be8648a7060acc477c66fc3cba25c37e7
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-03 14:13:58 +0000

    run experiments with building dataset

--- models/extract_features.py
+++ models/extract_features.py
@@ -46,11 +46,13 @@ DHS_MODELS = [
   #  'DHS_OOC_c_nl_random_b32_fc1.0_conv1.0_lr0001',
    # 'DHS_OOC_D_nl_random_b32_fc1.0_conv1.0_lr0001',
  #  '/DHS_OOC_E_nl_random_b32_fc1.0_conv1.0_lr0001',
-'DHS_OOC_A_nl_random_b_b32_fc1.0_conv1.0_lr0001',
-'DHS_OOC_B_nl_random_b_b32_fc1.0_conv1.0_lr0001',
-'DHS_OOC_C_nl_random_b_b32_fc1.0_conv1.0_lr0001',
-'DHS_OOC_D_nl_random_b_b32_fc1.0_conv1.0_lr0001',
-'DHS_OOC_E_nl_random_b_b32_fc1.0_conv1.0_lr0001',
+#'DHS_OOC_A_nl_random_b_b32_fc1.0_conv1.0_lr0001',
+#'DHS_OOC_B_nl_random_b_b32_fc1.0_conv1.0_lr0001',
+#'DHS_OOC_C_nl_random_b_b32_fc1.0_conv1.0_lr0001',
+#'DHS_OOC_D_nl_random_b_b32_fc1.0_conv1.0_lr0001',
+#'DHS_OOC_E_nl_random_b_b32_fc1.0_conv1.0_lr0001',
+'DHS_OOC_E_nl_random_b32_fc1.0_conv1.0_lr0001'
+
     # 'dhs_ooc/DHS_OOC_A_rgb_same_b64_fc0001_conv0001_lr001',
     # 'dhs_ooc/DHS_OOC_B_rgb_same_b64_fc001_conv001_lr0001',
     # 'dhs_ooc/DHS_OOC_C_rgb_same_b64_fc001_conv001_lr0001',
