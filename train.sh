c5796f9732bcde9a85241b9ec87a6eb2dc0555d8
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-03 08:08:42 +0000

    run experiments with building dataset

--- train.sh
+++ train.sh
@@ -13,7 +13,7 @@
 GPUS=1
 echo "Number of GPUs: "${GPUS}
 WRAP="python -m src.train2"
-JOBNAME="nl_D"
+JOBNAME="nl_E"
 LOG_FOLDER="/atlas/u/amna/logs/resnet18_logs/"
 echo ${WRAP}
 echo "Log Folder:"${LOG_FOLDER}
