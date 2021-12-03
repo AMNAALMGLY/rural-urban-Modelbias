07eee2314554482bb23b1a4e3059a5585925455f
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-03 10:56:23 +0000

    run experiments with building dataset

--- configs.py
+++ configs.py
@@ -50,7 +50,7 @@ args = Namespace(
 
     seed=123,
     experiment_name='DHS_OOC_E_nl_random',
-    out_dir=os.path.join(ROOT_DIR, 'outputs','dhs_buildings'),
+    out_dir=os.path.join(ROOT_DIR, 'outputs',),
     init_ckpt_dir=None,
     group=None,
 
