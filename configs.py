f3f9552ba8765f460321271fe202880eb2498c15
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-05 09:45:52 +0000

    run experiments with building dataset

--- configs.py
+++ configs.py
@@ -22,10 +22,10 @@ args = Namespace(
     epoch_thresh=150,
     patience=20,
 
-    lr=.01,  # lr0001         #0.0001 nl,ms
-    fc_reg=0.001,
+    lr=.001,  # lr0001         #0.0001 nl,ms
+    fc_reg=0.01,
     # fc01_conv01_lr0001        fc001_conv001_lr0001       fc001_conv001_lr001   fc001_conv001_lr01       fc01_conv01_lr001
-    conv_reg=0.001,
+    conv_reg=0.01,
 
     # data
 
@@ -37,7 +37,7 @@ args = Namespace(
     clipn=True,
     ooc=True,
     dataset='DHS_OOC',
-    fold='D',
+    fold='E',
     ls_bands='ms',
     nl_band=None,  # [None , merge , split]
     nl_label=None,  # [center, mean,None]
@@ -49,7 +49,7 @@ args = Namespace(
     # Experiment
 
     seed=123,
-    experiment_name='DHS_OOC_D_ms_samescaled',
+    experiment_name='DHS_OOC_E_ms_samescaled',
     out_dir=os.path.join(ROOT_DIR, 'outputs', 'dhs_ooc'),
     init_ckpt_dir=None,
     group=None,
