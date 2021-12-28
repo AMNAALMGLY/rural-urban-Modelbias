4ceccf32531b9ab3ef5535e07da39adb9679da2d
AMNAALMGLY <mnmnalmagly@gmail.com>
2021-12-28 22:22:58 +0000

    run experiments

--- src/trainer2.py
+++ src/trainer2.py
@@ -71,7 +71,8 @@ class Trainer:
             fc = nn.Linear(model.fc.in_features, num_outputs,bias=True)
             # initialization
             torch.nn.init.trunc_normal_(fc.weight.data, std=0.01)
-            torch.nn.init.constant_(fc.bias.data, 0.01)
+            #fc.bias.data.zero_()
+            #torch.nn.init.constant_(fc.bias.data, 0.01)
 
             model.fc = fc
 
