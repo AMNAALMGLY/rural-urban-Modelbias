6d48b1bdf23bc0e86d4d52a8c80344cc5a3fde43
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-01-09 16:00:59 +0000

    play around building dataset

--- preprocessing/ee_utils_building.py
+++ preprocessing/ee_utils_building.py
@@ -219,7 +219,7 @@ class AfricaBuildings:
         self.t = ee.FeatureCollection('GOOGLE/Research/open-buildings/v1/polygons')
         self.t_gte_070 = self.t.filter('confidence >= 0.70')
         
-        t_img = self.t_gte_070.reduceToImage(['confidence'], ee.Reducer.first()).unmask(0)
+        t_img = self.t_gte_070.reduceToImage(['area'], ee.Reducer.first()).unmask(0)
         t_img_rescaled = t_img.setDefaultProjection('EPSG:3857').reduceResolution(reducer=ee.Reducer.mean(), maxPixels=900)
         
         self.t_img = t_img_rescaled.rename(['buildings'])
