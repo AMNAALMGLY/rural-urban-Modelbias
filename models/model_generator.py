cbf966a02e073196414f98e8b37ffaa4cc7ddd91
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-01-25 15:34:43 +0000

    encoder

--- models/model_generator.py
+++ models/model_generator.py
@@ -48,7 +48,7 @@ class Encoder(nn.Module):
         features_img = self.resnet_bands(x['images'])[1] if 'images' in x else features_img
         features_b = self.resnet_build(x['buildings'])[1] if 'buildings' in x else features_b
         features_meta = self.Mlp(x[args.metadata[0]])[1] if args.metadata[0] in x else features_meta
-
+        print(features_img.device(),features_b.device,features_meta.device)
         # aggergation:
         features = features_img + features_b + features_meta
         print('features shape together :', features.shape)
