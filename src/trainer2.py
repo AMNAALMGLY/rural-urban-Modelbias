f5809a2664e854cf40ef4199a8cb1283b5a33b6b
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-01-29 22:23:17 +0000

    encoder country meta data

--- src/trainer2.py
+++ src/trainer2.py
@@ -161,7 +161,7 @@ class Trainer:
                         batch[0][meta] = tf.map_fn(fn=lambda t: DHS_COUNTRIES.index(t), elems=batch[0][meta],fn_output_signature=tf.int32)
                         batch[0][meta]=tf.reshape(batch[0][meta],[-1,1])
                         #print(batch[0][meta],batch[0][meta].shape)
-                    x[meta] = torch.tensor(batch[0][meta], )
+                    x[meta] = torch.tensor(batch[0][meta].numpy(), )
             target = torch.tensor(batch[0]['labels'], )
             target = target.type_as(self.model.fc.weight)
             x['buildings'] = torch.tensor(batch[1]['buildings'], )
