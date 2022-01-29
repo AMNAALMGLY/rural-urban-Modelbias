7484bf61b9e9ce56c3b2eb489bdd6f171f6201d1
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-01-29 00:52:20 +0000

    encoder intersample attention

--- models/model_generator.py
+++ models/model_generator.py
@@ -88,8 +88,9 @@ def attention(query, key, value, dropout=None):
     # value: bs, n, embed_dim
     d_k = query.size(-1)
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
-
+    print('scores shape',scores.shape)
     p_attn = F.softmax(scores, dim=-1)
+    print('softmax',p_attn)
     if dropout is not None:
         p_attn = dropout(p_attn)  # bs , n , n
     output = torch.matmul(p_attn, value)  # bs, n , embed_dim
