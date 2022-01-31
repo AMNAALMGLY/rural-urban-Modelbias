6ea557110e5f8835284aa15081a0f87f89e3a5a3
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-01-31 12:40:03 +0000

    encoder multihead

--- models/resnet.py
+++ models/resnet.py
@@ -261,7 +261,7 @@ class ResNet(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
         # Attention
-        self.attn = SE_Block(c=512)
+        #self.attn = SE_Block(c=512)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -365,7 +365,7 @@ def _resnet(
         # attn_weights=["attn.gamma", "attn.query_conv.weight", "attn.query_conv.bias", "attn.key_conv.weight",
         #             "attn.key_conv.bias", "attn.value_conv.weight", "attn.value_conv.bias"]
         #attn_weights=["attn.excitation.0.weight","attn.excitation.2.weight" ]
-        '''
+        """
         attn_weights = [
                         "layer1.0.se.excitation.0.weight", "layer1.0.se.excitation.2.weight",
                         "layer1.1.se.excitation.0.weight",
@@ -376,7 +376,7 @@ def _resnet(
                         "layer3.1.se.excitation.2.weight", "layer4.0.se.excitation.0.weight",
                         "layer4.0.se.excitation.2.weight", "layer4.1.se.excitation.0.weight",
                         "layer4.1.se.excitation.2.weight"]
-        '''
+        """
         state_dict = load_state_dict_from_url(model_urls[arch], progress=progress)
         state_dict['conv1.weight'] = nn.Parameter(
             init_first_layer_weights(in_channels, state_dict['conv1.weight'], args.hs_weight_init))
