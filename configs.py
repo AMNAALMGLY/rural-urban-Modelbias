741187da54d6cfbc7492a8f149d3657222fe4f64
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-01-31 00:16:14 +0000

    encoder multihead

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ args = Namespace(
     # Model
 
     model_name=defaultdict(resnet_bands='resnet18',resnet_build='resnet18',Mlp='mlp'),
-        self_attn='multihead', #choices : [vanilla, intersample , multihead]
+        self_attn=None, #choices : [vanilla, intersample , multihead]
     hs_weight_init='random',  # [same, samescaled,random]
     model_init=['imagenet','imagenet',None],
     imagenet_weight_path='/atlas/group/model_weights/imagenet_resnet18_tensorpack.npz',
