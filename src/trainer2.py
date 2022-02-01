bdafd281e17744e8df611ba4196cd23960eebf03
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-02-01 04:26:40 +0000

    encoder multihead

--- src/trainer2.py
+++ src/trainer2.py
@@ -150,8 +150,9 @@ class Trainer:
                 # 2 bands split them inot seperate inputs
                 # assumes for now it is only merged nl_bands
                 x[args.ls_bands] = torch.tensor(batch[0]['images'][:, :,:, :-1], )
-                print('ls_band shape',x[args.ls_bands].shape)
+
                 x[args.nl_band] = torch.tensor(batch[0]['images'][:, :,:, -1], )
+                print('ls_band shape', x[args.nl_band].shape)
             elif args.ls_bands or args.nl_band:
                 # only one type of band
                 x['images'] = torch.tensor(batch[0]['images'])
@@ -170,8 +171,8 @@ class Trainer:
             if args.ls_bands and args.nl_band:
                 # 2 bands split them to seperate inputs
                 # assumes for now it is only merged nl_bands
-                x[args.ls_bands] = torch.tensor(batch['images'][:, :, :-1], )
-                x[args.nl_band] = torch.tensor(batch['images'][:, :, -1], )
+                x[args.ls_bands] = torch.tensor(batch['images'][:, :,:, :-1] )
+                x[args.nl_band] = torch.tensor(batch['images'][:, :,:, :-1] )
             elif args.ls_bands or args.nl_band:
                 # only one type of band
                 x['images'] = torch.tensor(batch['images'])
