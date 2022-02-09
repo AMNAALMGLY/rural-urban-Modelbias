9a78e6abf711860da96d1f2ca12b75c27e2d97e3
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-02-09 21:52:38 +0000

    NL urban only

--- src/trainer2.py
+++ src/trainer2.py
@@ -730,7 +730,7 @@ class Trainer:
 
             x['buildings']=torch.tensor( input[1]['buildings'])
             x={key :value.reshape(-1,value.shape[-1],value.shape[-2],value.shape[-3])for key , value in x.items()}
-            x={key:value.as_type(model.fc.weight) for key , value in x.items()}
+            x={key:value.type_as(model.fc.weight) for key , value in x.items()}
             input =x
             if device is not None:
                 input = input.to(device)
