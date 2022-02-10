f00378ef7e002ce609478f6723c8229233a47061
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-02-10 12:06:27 +0000

    NL b merge

--- src/trainer2.py
+++ src/trainer2.py
@@ -667,10 +667,10 @@ class Trainer:
                 # 'scheduler': ExponentialLR(opt,
                  #           gamma=args.lr_decay),
                 # 'scheduler':torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, T_0=args.max_epochs),
-               'scheduler': optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(opt, warmup_epochs=10,
-                                                                                   max_epochs=500,
-                                                                                   warmup_start_lr=1e-7),
-                 #'scheduler': torch.optim.lr_scheduler.StepLR(opt, step_size=1, gamma=args.lr_decay, ),
+              # 'scheduler': optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(opt, warmup_epochs=10,
+               #                                                                    max_epochs=500,
+                #                                                                   warmup_start_lr=1e-7),
+                 'scheduler': torch.optim.lr_scheduler.StepLR(opt, step_size=1, gamma=args.lr_decay, ),
                 # 'scheduler':torch.optim.lr_scheduler.ReduceLROnPlateau(opt, 'min'),
                 'swa_scheduler': torch.optim.swa_utils.SWALR(opt, anneal_strategy="cos", anneal_epochs=5, swa_lr=0.05)
 
