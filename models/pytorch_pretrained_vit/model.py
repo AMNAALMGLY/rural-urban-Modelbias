6b071721230aef4a649e08532beecd70b587ab55
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-03-27 16:34:06 +0000

    vit

--- models/pytorch_pretrained_vit/model.py
+++ models/pytorch_pretrained_vit/model.py
@@ -161,11 +161,15 @@ class ViT(nn.Module):
         """
         b, c, fh, fw = x.shape
         x = self.patch_embedding(x)  # b,d,gh,gw
+        print(x.shape)
         x = x.flatten(2).transpose(1, 2)  # b,gh*gw,d
+        print(x.shape)
         if hasattr(self, 'class_token'):
             x = torch.cat((self.class_token.expand(b, -1, -1), x), dim=1)  # b,gh*gw+1,d
+            print(x.shape)
         if hasattr(self, 'positional_embedding'):
-            x = self.positional_embedding(x)  # b,gh*gw+1,d 
+            x = self.positional_embedding(x)  # b,gh*gw+1,d
+
         x = self.transformer(x)  # b,gh*gw+1,d
         if hasattr(self, 'pre_logits'):
             x = self.pre_logits(x)
