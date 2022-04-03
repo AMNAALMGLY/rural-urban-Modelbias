a9b05f4ef79839bb9876a9ad99dff3633316ac4a
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-04-03 14:00:43 +0000

    positional encoding fixing bugs

--- utils/utils.py
+++ utils/utils.py
@@ -184,7 +184,7 @@ def get_sustain_labels(lat, lon, label):
     # startegy 2 : read them directly and return them directly
     path=os.path.join(os.getcwd(),'batchers','dhs_final_labels.csv')
     dataframe=pd.read_csv(path)
-    match = dataframe[(dataframe['lat'] == lat) & (dataframe['lon'] == lon)]
+    match =dataframe[(dataframe['lon']==lon) & (dataframe['lat']==lat)]
     print(lat,lon)
 
     if len(match.index)==0:
