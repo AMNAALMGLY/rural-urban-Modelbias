85612492ba5e273ad38eee7d38908fa2ece91d59
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-04-25 01:22:08 +0000

    attn debug

--- models/model_generator.py
+++ models/model_generator.py
@@ -183,8 +183,8 @@ class Encoder(nn.Module):
             else:
                 self.PE = PositionalEncoding2D(self.fc_in_dim)
             if self.self_attn=='torch':
-                self.multi_head_adapt=nn.MultiheadAttention(embed_dim=self.fc_in_dim,num_heads=
-                                                   1,dropout=0.1)
+                self.multi_head_adapt=MultiheadAttention(input_dim=self.fc_in_dim,embed_dim=self.fc_in_dim,num_heads=
+                                                   1)
             else:
                    self.multi_head_adapt = MultiHeadedAttentionAdapt(h=1, d_model=self.fc_in_dim, w=self.self_attn)
             self.layer_adapt = EncoderLayer(size=self.fc_in_dim, self_attn=self.multi_head_adapt,
@@ -410,8 +410,60 @@ class MultiHeadedAttentionAdapt(nn.Module):
         assert tuple(x.shape) == (nbatches, nPatches, self.d_k * self.h)
 
         return self.dropout(x)  # bs , n , d_model
+def scaled_dot_product(q, k, v, mask=None):
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = F.softmax(attn_logits, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+class MultiheadAttention(nn.Module):
 
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super().__init__()
+        assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
 
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        self.attn=None
+
+        # Stack all weight matrices 1...h together for efficiency
+        # Note that in many implementations you see "bias=False" which is optional
+        self.qkv_proj = nn.Linear(input_dim, 3*embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        # Original Transformer initialization, see PyTorch documentation
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, embed_dim = x.size()
+        qkv = self.qkv_proj(x)
+
+        # Separate Q, K, V from linear output
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3*self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3) # [Batch, Head, SeqLen, Dims]
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        # Determine value outputs
+        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
+        values = values.reshape(batch_size, seq_length, embed_dim)
+        o = self.o_proj(values)
+        self.attn=attention
+
+        if return_attention:
+            return o, attention
+        else:
+            return o
 def clones(module, N):
     "Produce N identical layers."
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
