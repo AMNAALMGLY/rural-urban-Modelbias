79d6b83caf86e51e440719a6553bac295817421f
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-05-06 16:36:39 +0000

    tuning

--- src/trainer2.py
+++ src/trainer2.py
@@ -757,7 +757,7 @@ class Trainer:
         return avg_valid_loss
     def run_optuna(self):
         study = optuna.create_study(direction="minimize")
-        study.optimize(self.train_optuna, n_trials=50)
+        study.optimize(self.train_optuna, n_trials=5)
 
         pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
         complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
